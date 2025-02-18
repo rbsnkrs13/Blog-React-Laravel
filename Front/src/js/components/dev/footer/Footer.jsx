7be3a158376bd2d270a19a2b00f787733be8235d
Antonio <antonio.gil.ozlane@gmail.com>
2025-02-18 14:44:52 +0100

    footer.jsx v0.01

--- Front/src/js/components/dev/footer/Footer.jsx
+++ Front/src/js/components/dev/footer/Footer.jsx
@@ -1,13 +1,14 @@
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer style={{ padding: '20px', textAlign: 'center', backgroundColor: '#f8f8f8' }}>
       <ul style={{ listStyle: 'none', padding: 0 }}>
         <li style={{ display: 'inline', margin: '0 15px' }}>
-          <a href="/cookies" style={{ textDecoration: 'none', color: '#333' }}>Cookies</a>
+            <Link to="/cookies">Cookies</Link>
         </li>
         <li style={{ display: 'inline', margin: '0 15px' }}>
-          <a href="/privacidad" style={{ textDecoration: 'none', color: '#333' }}>Privacidad</a>
+            <Link to="/privacidad">Privacidad</Link>
         </li>
       </ul>
     </footer>
