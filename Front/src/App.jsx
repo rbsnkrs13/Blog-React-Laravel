f2e9feb2095921e305da4aa6a1e91fd08c819936
JMarotoM <116755685+JMarotoM@users.noreply.github.com>
2025-02-18 12:01:40 +0100

    Instalacion de daisy y tailwind

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -3,7 +3,11 @@ import Prueba from "./js/components/dev/prueba/prueba";
 import Counter from "./js/components/dev/counter/Counter";
 
 function App() {
-  return <Counter />;
+  // return <Counter />;
+  return (
+    <button class="btn btn-primary">Button</button>
+
+  )
 }
 
 export default App;
