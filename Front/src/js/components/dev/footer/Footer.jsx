fc57eda736e99d11e8dc7243f575b95c3c1d987e
Antonio <antonio.gil.ozlane@gmail.com>
2025-02-18 14:55:32 +0100

    css font

--- Front/src/js/components/dev/footer/Footer.jsx
+++ Front/src/js/components/dev/footer/Footer.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
+import "./Footer.css";
 
 const Footer = () => {
   return (
     <footer style={{ padding: '20px', textAlign: 'center', backgroundColor: '#f8f8f8' }}>
       <ul style={{ listStyle: 'none', padding: 0 }}>
-        <li style={{ display: 'inline', margin: '0 15px' }}>
+        <li className="link" style={{ display: 'inline', margin: '0 15px' }}>
             <Link to="/cookies">Cookies</Link>
         </li>
-        <li style={{ display: 'inline', margin: '0 15px' }}>
+        <li className="link" style={{ display: 'inline', margin: '0 15px' }}>
             <Link to="/privacidad">Privacidad</Link>
         </li>
       </ul>
