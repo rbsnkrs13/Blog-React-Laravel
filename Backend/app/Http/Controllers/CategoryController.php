ad8b869a7394ba54cca7bd4e584f7bd97f87ec42
MarcosMIzquierdoLinares <marcos.izquierdo.linares@gmail.com>
2025-02-18 09:52:29 +0100

    Cambio de idioma, tabla category

--- Backend/app/Http/Controllers/CategoryController.php
+++ Backend/app/Http/Controllers/CategoryController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Categoria;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
-class CategoriaController extends Controller
+class CategoryController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -34,7 +34,7 @@ class CategoriaController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Categoria $categoria)
+    public function show(Category $category)
     {
         //
     }
@@ -42,7 +42,7 @@ class CategoriaController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Categoria $categoria)
+    public function edit(Category $category)
     {
         //
     }
@@ -50,7 +50,7 @@ class CategoriaController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Categoria $categoria)
+    public function update(Request $request, Category $category)
     {
         //
     }
@@ -58,7 +58,7 @@ class CategoriaController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Categoria $categoria)
+    public function destroy(Category $category)
     {
         //
     }
