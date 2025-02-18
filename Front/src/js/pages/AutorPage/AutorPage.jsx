5533c4a4e1d72a4f03aeab4aa6fda719bae23716
Angel Sanchez <angel.s.p2064@gmail.com>
2025-02-18 11:53:37 +0100

    archivos de paginas y rutas completas

---
+++ Front/src/js/pages/AutorPage/AutorPage.jsx
@@ -0,0 +1,9 @@
+const AutorPage = () => {
+  return (
+    <div>
+      <h1>Autor</h1>
+    </div>
+  );
+};
+
+export default AutorPage;
---
+++ Front/src/js/pages/LogInPage/LogInPage.jsx
@@ -0,0 +1,10 @@
+const LogInPage = () => {
+  return (
+    <div>
+      <h1>Log In</h1>
+      {/* <LogInForm /> */}
+    </div>
+  );
+};
+
+export default LogInPage;
