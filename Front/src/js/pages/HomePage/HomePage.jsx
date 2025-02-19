66ba80c5df95bff0c593b510849b508e4ba51144
MarioSaugar <mario.saugarsarabia@gmail.com>
2025-02-19 13:08:43 +0100

    add imports

--- Front/src/js/pages/HomePage/HomePage.jsx
+++ Front/src/js/pages/HomePage/HomePage.jsx
@@ -1,10 +1,14 @@
+import CreatePost from "../../components/dev/createPost/createPost";
 import Title from "../../components/dev/Title/Title";
+import BackToTop from "../../components/dev/BackToTop/BackToTop";
 
 const HomePage = () => {
   return (
     <div>
       {/* f_size y h_num son opcionales */}
       <Title texto="Home Page" f_size = {40} h_num={1} />
+      <BackToTop />
+      <CreatePost />
     </div>
   );
 };
