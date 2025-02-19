1afa02ff45bd60fd517fafebb299868fd6fe73c8
Antonio <antonio.gil.ozlane@gmail.com>
2025-02-19 12:49:09 +0100

    comm

--- Front/src/js/pages/HomePage/HomePage.jsx
+++ Front/src/js/pages/HomePage/HomePage.jsx
@@ -3,7 +3,7 @@ import Title from "../../components/dev/Title/Title";
 const HomePage = () => {
   return (
     <div>
-      // f_size y h_num son opcionales
+      {/* f_size y h_num son opcionales */}
       <Title texto="Home Page" f_size = {40} h_num={1} />
     </div>
   );
