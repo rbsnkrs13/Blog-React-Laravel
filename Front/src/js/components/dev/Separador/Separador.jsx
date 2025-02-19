ea9efd11e9ab028d65071ac0389d6ac0f2f56129
JMarotoM <116755685+JMarotoM@users.noreply.github.com>
2025-02-19 12:28:14 +0100

    Separador hecho

--- Front/src/js/components/dev/Separador/Separador.jsx
+++ Front/src/js/components/dev/Separador/Separador.jsx
@@ -1,17 +1,16 @@
 import { useState, useEffect } from 'react';
-import { Container, } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import separador from '../../../../assets/separador.svg'; // Importa el archivo SVG
 import './Separador.css';
 
-const SearchPage = () => {
-    const [isMobile, setIsMobile] = useState(window.innerWidth < 768); // Estado para detectar si es móvil
-
- 
+const Separador = () => {
+   // const [isMobile, setIsMobile] = useState(window.innerWidth < 768); // Estado para detectar si es móvil
 
     return (
         <Container>
-           
+            <img src={separador} alt="Separador" className="separador-img" />
         </Container>
     );
 };
 
-export default SearchPage;
+export default Separador;
--- Front/src/js/pages/HomePage/HomePage.jsx
+++ Front/src/js/pages/HomePage/HomePage.jsx
@@ -1,10 +1,13 @@
 import Article_finder from "../../components/dev/article_finder/Article_finder";
+import Separador from "../../components/dev/Separador/Separador";
 
 const HomePage = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <Separador  />
       <Article_finder />
+      <Separador  />
     </div>
   );
 };
