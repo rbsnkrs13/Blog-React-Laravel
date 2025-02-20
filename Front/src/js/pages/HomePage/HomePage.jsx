da809645f3611f0e636a54f4d431226111678150
Angel Sanchez <angel.s.p2064@gmail.com>
2025-02-20 10:13:09 +0100

    odio github

--- Front/src/js/pages/HomePage/HomePage.jsx
+++ Front/src/js/pages/HomePage/HomePage.jsx
@@ -7,7 +7,7 @@ const HomePage = () => {
   return (
     <div>
       {/* f_size y h_num son opcionales */}
-      <Title texto="Home Page" f_size = {40} h_num={1} />
+      <Title texto="Home Page" f_size={40} h_num={1} />
       <BackToTop />
       <CreatePost />
 
