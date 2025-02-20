239565d0346226ffd34a769d003e894bb5414bdc
Hmg <HmgDevWorks@gmail.com>
2025-02-20 12:56:03 +0100

    Login form with Daisy

--- Front/src/js/components/dev/login/LoginForm.jsx
+++ Front/src/js/components/dev/login/LoginForm.jsx
@@ -1,124 +1,98 @@
-import React, { useState } from 'react';
-import { Link } from "react-router-dom";
-import Logo from "./../../../../assets/logo_pluma.svg";
-// import { Form, Button, Row, Col, Card, Modal } from 'react-bootstrap';
+import { useState } from 'react';
+import logo from '../../../../assets/logo_pluma.svg';
 import './LoginForm.css';
+import userService from '../../../services/userService';
 
-const LoginForm = () => {
-    const [showModal, setShowModal] = useState(false);
+export default function LoginForm() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
 
-    const handleShowModal = () => setShowModal(true);
-    const handleCloseModal = () => setShowModal(false);
+    const [login, setLogin] = useState(true);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // console.log('username:', username);
+        // console.log('password:', password);
+        // console.log('email:', email);
+        // console.log('confirmPassword:', confirmPassword);
+        if (!login && password !== confirmPassword) {
+            alert('Las contraseñas no coinciden');
+            return;
+        }
+
+        const data = login ? { username, password } : { username, password, email };
+        const request = login ? userService.getOneUser(data) : userService.createUser(data);
+
+        request
+            .then(response => {
+            console.log('Success:', response.data);
+            })
+            .catch(error => {
+            console.error('Error:', error);
+            });
+    }
+
+    function handleUsernameChange(e) {
+        setUsername(e.target.value);
+    }
+    function handlePasswordChange(e) {
+        setPassword(e.target.value);
+    }
+    function handleEmailChange(e) {
+        setEmail(e.target.value);
+    }
+    function handleConfirmPasswordChange(e) {
+        setConfirmPassword(e.target.value);
+    }
 
     return (
-        <>
-            <Card className="cardForm">
-                <Card.Header className="headerForm d-flex align-items-center">
-                    <img src={Logo} alt="Logo" />
-                    <h1 className="text-center">C -Blog</h1>
-                </Card.Header>
-                <Card.Body>
-                    <Form>
-                        <Row className="mb-3">
-                            <Col xs={4} lg={5} className="d-flex align-items-center justify-content-center">
-                                <Form.Label className="formLabel" for="name_user">Usuario:</Form.Label>
-                            </Col>
-                            <Col xs={8} lg={7}>
-                                <Form.Control type="text" id="name_user" name= "name_user" />
-                            </Col>
-                        </Row>
-
-                        <Row className="mb-3">
-                            <Col xs={5} className="d-flex align-items-center justify-content-center">
-                                <Form.Label className="formLabel" for="password_user">Contraseña:</Form.Label>
-                            </Col>
-                            <Col xs={7}>
-                                <Form.Control type="password" id="password_user" name= "password_user" />
-                            </Col>
-                        </Row>
-
-                        <Row className="mb-3 d-flex justify-content-center">
-                            <Col xs={5} lg={4} className="d-flex align-items-center justify-content-center">
-                                <Form.Check type="checkbox" id="rememberMe" label="Recuérdame" name= "rememberMe" />
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col xs={12} className="d-flex align-items-center justify-content-center">
-                                <Button type="submit" className="botonForm mt-3">
-                                    Enviar
-                                </Button>
-                            </Col>
-                        </Row>
-                    </Form>
-                </Card.Body>
-                <Card.Footer className="footerForm">
-                    <Row className="mb-1">
-                        <p>También puedes: <Link className="crearCuenta" onClick={handleShowModal}>Crear una cuenta nueva</Link></p>
-                    </Row>
-                </Card.Footer>
-            </Card>
-              {/* MODAL */}
-            <Modal show={showModal} onHide={handleCloseModal} className="cardForm">
-                <Modal.Header closeButton className="headerForm">
-                    <img src={Logo} alt="Logo" />
-                    <h1 className="text-center">C -Blog</h1>
-                </Modal.Header>
-                <Modal.Body>
-                    <Form>
-                        <Row className="mb-3 d-flex align-items-center">
-                            <Col xs={4} lg={5} className="d-flex align-items-center">
-                                <Form.Label className="formLabel" for="name_user">Usuario:</Form.Label>
-                            </Col>
-                            <Col xs={8} lg={7}>
-                                <Form.Control type="text" id="name_user" name= "name_user" />
-                            </Col>
-                        </Row>
-
-                        <Row className="mb-3 d-flex align-items-center">
-                            <Col xs={4} lg={5} className="d-flex align-items-center">
-                                <Form.Label className="formLabel" for="password_user">Contraseña:</Form.Label>
-                            </Col>
-                            <Col xs={8} lg={7}>
-                                <Form.Control type="password_user" name= "password_user" />
-                            </Col>
-                        </Row>
-
-                        <Row className="mb-3 d-flex align-items-center">
-                            <Col xs={4} lg={5} className="d-flex align-items-center">
-                                <Form.Label className="formLabel" for="password_user_confirmation">Repite Contraseña:</Form.Label>
-                            </Col>
-                            <Col xs={8} lg={7}>
-                                <Form.Control type="password" id="password_user_confirmation" name= "password_user_confirmation" />
-                            </Col>
-                        </Row>
-
-                        <Row className="mb-3 d-flex align-items-center">
-                            <Col xs={4} lg={5} className="d-flex align-items-center">
-                                <Form.Label className="formLabel" for="email_user">Correo electrónico:</Form.Label>
-                            </Col>
-                            <Col xs={8} lg={7}>
-                                <Form.Control type="email" id="email_user" name= "email_user" />
-                            </Col>
-                        </Row>
-                        
-                        <Row className="mb-3 d-flex w-100">
-                            <Col xs={12}>
-                                <Form.Check type="checkbox" id="terms" name= "terms" label="Acepto los términos de servicio, política de privacidad y servicio de notificaciones" />
-                            </Col>
-                        </Row>
-                        <Row className="mb-3">
-                            <Col xs={4} className="d-flex align-items-center justify-content-center w-100 mx-auto">
-                              <Button type="submit" className="botonForm mt-3">
-                                  Registrarse
-                              </Button>   
-                          </Col>
-                        </Row>
-                    </Form>
-                </Modal.Body>
-              
-            </Modal>
-        </>
+        <form className="p-4 bg-white shadow-md rounded-lg" onSubmit={handleSubmit}>
+                <div className="text-left img-container">
+                    <img src={logo} alt="Logo" />
+                    <h1 className="text-2xl font-bold">C-Blog</h1>
+                </div>
+                <div className='form-container'>
+                <LoginFormInput id="username" label="Usuario" type="text" placeholder="Usuario" onChange={handleUsernameChange}/>
+                <LoginFormInput id="password" label="Contraseña" type="password" placeholder="Contraseña" onChange={handlePasswordChange} />
+                {!login && 
+                    <><LoginFormInput id="password" label="Confirmar contraseña" type="password" placeholder="Confirmar contraseña" onChange={handleConfirmPasswordChange} />
+                    <LoginFormInput id="email" label="Email" type="text" placeholder="Email" onChange={handleEmailChange} /></>}
+                {login && <div className="mb-4">
+                    <label className="inline-flex items-center">
+                        <input type="checkbox" className="form-checkbox checkbox " />
+                        <span className="ml-2">Recuerdame</span>
+                    </label>
+                </div>}
+                <button 
+                    type="submit" 
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                    {login? "Iniciar Sesion" : "Registrarse"}
+                </button>
+                <hr className="my-4"/>
+                <p>
+                    Tambien puedes: <button type="button" className="text-blue-500 underline" onClick={() => setLogin(login => !login)}>{login? "Crear una cuenta" : "Iniciar Sesion"}</button>
+                </p>
+                </div>
+        </form>
     );
-};
 
-export default LoginForm;
+}
+
+function LoginFormInput({ id, label, type, placeholder, onChange }) {
+    return (
+        <div className="mb-4 input-container">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor={id}>
+                {label}
+            </label>
+            <input 
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                id={id} 
+                type={type} 
+                placeholder={placeholder} 
+                onChange={onChange}
+            />
+        </div>
+    );
+}
