e3fb8e87f52fc2ca891d976d34c8a74a498da6ac
JMarotoM <116755685+JMarotoM@users.noreply.github.com>
2025-02-21 10:30:06 +0100

    Reparados los carouseles
    This reverts commit fbd1832380ec979320ff5859d79049e040e5d5a6, reversing
    changes made to ec2308a6634ee537b10734027950b9226c441866.

--- Front/src/js/components/dev/carousel/NewsCarousel.jsx
+++ Front/src/js/components/dev/carousel/NewsCarousel.jsx
@@ -3,10 +3,8 @@ import Post from './Post';
 import './NewsCarousel.css';
 
 export default function NewsCarousel({ newsItems }) {
-    // Función para agrupar los elementos de noticias en el actual, el anterior y el siguiente
-
     return (
-        <div className="carousel">
+        <div className="carousel-news">
             {newsItems.map((item, index) => (
                 <Post key={index} item={item} index={index} totalItems={newsItems.length} />
             ))}
