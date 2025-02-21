28c8a5f937cbaa1ed19812b44155b700d490e1c6
Raquel Martínez Jiménez <raquelmj1995@gmail.com>
2025-02-21 12:28:46 +0100

    ComponenteAutoresImportado

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import NavigationFinal from "./js/components/dev/NavigationFinal/NavigationFinal";
 import AppRoutes from "./js/router/AppRoutes";
+import Authors from "./js/components/dev/authors/Authors";
 
 import Footer from "./js/components/dev/footer/Footer";
 
@@ -12,6 +13,7 @@ function App() {
       <NavigationFinal />
       <main>
         <AppRoutes />
+        <Authors/>
       </main>
       <Footer />
     </div>
