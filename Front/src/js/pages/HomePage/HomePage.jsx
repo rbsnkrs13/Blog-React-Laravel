259212307b546e28d06e5176754c75ae64143102
MarioSaugar <mario.saugarsarabia@gmail.com>
2025-02-24 13:17:40 +0100

    todo bien

--- Front/src/js/pages/HomePage/HomePage.jsx
+++ Front/src/js/pages/HomePage/HomePage.jsx
@@ -3,18 +3,11 @@ import Title from "../../components/dev/title/TitleBlog";
 import BackToTop from "../../components/dev/backToTop/BackToTop";
 import LikeButton from "../../components/dev/likeButton/LikeButton";
 import Categorycarousel from "../../components/dev/Categorycarousel/Categorycarousel";
-<<<<<<< HEAD
 import Separador from "../../components/dev/Separador/Separador";
 import ArticleFinder from "../../components/dev/article_finder/ArticleFinder";
 import CategoryFinder from "../../components/dev/categoryFinder/CategoryFinder";
 import Authors from "../../components/dev/authors/authors";
-=======
-import Separador from "../../components/dev/separador/Separador";
-import ArticleFinder from "../../components/dev/article_finder/Article_finder_daisy";
-// import Authors from "../../components/dev/authors/Authors";
-import NewsCarousel from "../../components/dev/carousel/NewsCarousel";
 import Counter from "../../components/dev/counter/Counter";
->>>>>>> a139f8c6ce0867351993e608456bc1875dc68d16
 
 const HomePage = () => {
   return (
