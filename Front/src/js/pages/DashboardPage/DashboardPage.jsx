d7a61b68a223cea551105b96beaa3d32cdacea5a
AdrianBusquets <125498364+AdrianBusquets@users.noreply.github.com>
2025-02-24 11:46:15 +0100

    Dashboard casi terminada

--- Front/src/js/pages/DashboardPage/DashboardPage.jsx
+++ Front/src/js/pages/DashboardPage/DashboardPage.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Circle_graphic from "../../components/dev/circle_graphic/Circle_graphic";
 import Separador from "../../components/dev/separador/Separador";
+import TooltipGraphic from "../../components/dev/TooltipGraph/TooltipGraph";
+import AreaChart from "../../components/dev/Areachart/Areachart";
 import "./DashboardPage.css";
 
 export default function DashboardPage() {  
@@ -10,10 +12,10 @@ export default function DashboardPage() {
         <h1 className="dashboardTitle">Dashboard</h1>
         <Separador className="separadorDashboard" />
         <main className="dashboardGraphics">
-        <Routes>
-            <Route path={"/"} element={<Circle_graphic className="circleGraphicDashboard"/>} />
-        </Routes>
+            <TooltipGraphic className="tooltipGraphicDashboard dGraphic"/>
+            <Circle_graphic className="circleGraphicDashboard dGraphic"/>
+            <AreaChart className="areaChartDashboard dGraphic"/>
         </main>
         </div>
     );
-}
+} 
--- Front/src/js/pages/HomePage/HomePage.jsx
+++ Front/src/js/pages/HomePage/HomePage.jsx
@@ -7,22 +7,19 @@ import Separador from "../../components/dev/separador/Separador";
 import ArticleFinder from "../../components/dev/article_finder/Article_finder_daisy";
 // import Authors from "../../components/dev/authors/Authors";
 import NewsCarousel from "../../components/dev/carousel/NewsCarousel";
-import ArticleFinder from "../../components/dev/article_finder/Article_finder_daisy";
 import Counter from "../../components/dev/counter/Counter";
 
 const HomePage = () => {
   return (
     <div>
       {/* f_size y h_num son opcionales */}
-      <Title texto="Home Page" f_size={40} h_num={1} />
+      <Title texto="C-Blog, donde el limite de las ideas, lo pones tú" f_size={43} h_num={1} />
       <Separador />
       <Categorycarousel />
       <Separador />
       <ArticleFinder />
       <Separador />
-      <ArticleFinder /> 
-      <Separador />
-      {/* <Authors/> */}
+      <Authors/>
       <Separador />
       <Counter />
       <BackToTop />
