db83b0f45754a94120e6b52b136c88d43e90a198
PabloDelgadoSoto <pablodelsot@gmail.com>
2025-02-24 12:28:16 +0100

    Cambio en contraseña de pepe

--- Backend/database/seeders/userSeeder.php
+++ Backend/database/seeders/userSeeder.php
@@ -16,7 +16,7 @@ class userSeeder extends Seeder
         User::create([
             'name_user' => 'Pepe',
             'email_user' => 'pepe@gmail.com',
-            'password_user' => 'djewrukhkdew3',
+            'password_user' => '123456',
             'name_lastName' => 'Botas',
             'bio' => 'bifidus con fibra'
         ])->assignRole(['admin']);
