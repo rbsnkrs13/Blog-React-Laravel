0d63988e85200931a685ea980ae5402267a26875
Angel Sanchez <angel.s.p2064@gmail.com>
2025-02-24 14:55:38 +0100

    vamos a ello

--- Front/src/js/pages/NovedadesPage/NovedadesPage.jsx
+++ Front/src/js/pages/NovedadesPage/NovedadesPage.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from 'react';
 import "./NovedadesPage.css";
 import postService from '../../services/postService';
-// import Post from './Post';
-// import './NewsCarousel.css';
 import Loader from '../../components/dev/Loader/Loader';
 import DetallesBlog from '../../components/dev/DetallesBlog/DetallesBlog';
+import { redirect } from 'react-router-dom';
 
 const NovedadesPage = () => {
   const [newsItems, setNewsItems] = useState([]);
+  const [currentSlide, setCurrentSlide] = useState(0); // Estado para controlar el índice del slide actual
 
   useEffect(() => {
     loadNewPost();
@@ -17,30 +17,48 @@ const NovedadesPage = () => {
     postService
       .getLastTenPost()
       .then(({ data }) => {
-        setNewsItems(data)
+        setNewsItems(data);
       })
+      .catch(err => console.log(err));
+  };
 
-      .catch(err => console.log(err))
-  }
-
-
+  // Función para cambiar el slide
+  const handleNavigation = (direction) => {
+    if (direction === 'next') {
+      setCurrentSlide((prev) => (prev + 1) % newsItems.length);
+      redirect(`novedades#slide${currentSlide}`) // Navegar al siguiente slide
+    } else if (direction === 'prev') {
+      setCurrentSlide((prev) => (prev - 1 + newsItems.length) % newsItems.length); // Navegar al slide anterior
+      redirect(`novedades#slide${currentSlide}`) // Navegar al siguiente slide 
+    }
+  };
 
   return (
     <div className='novedades-page'>
       <h1 className='novedades'>Novedades:</h1>
-      <div className="carousel carousel-center bg-neutral rounded-box max-w-md space-x-4 p-4 carousel-news">
+      <div className="carousel w-full carousel-news">
         {newsItems && newsItems.length > 0 ? (
           newsItems.map((item, index) => (
-            <div className="carousel-item-news " key={index}>
-              <DetallesBlog blog={item} />
+            <div
+              key={item.id}
+              id={`slide${item.id}`}
+              className={`carousel-item relative w-full ${index === currentSlide ? 'block' : 'hidden'}`}
+            >
+              <DetallesBlog className="w-full" blog={item} />
             </div>
           ))
         ) : (
           <Loader />
         )}
-      </div>
 
+        {/* Navigation buttons */}
+        <div className="botones-novedades absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+          <button onClick={() => handleNavigation('prev')} className="btn btn-circle">❮</button>
+          <button onClick={() => handleNavigation('next')} className="btn btn-circle">❯</button>
+        </div>
+      </div>
     </div>
   );
 };
+
 export default NovedadesPage;
