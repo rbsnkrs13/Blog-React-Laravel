e87bc836f8fc2996dc7d423d453f6e00c8d99177
JMarotoM <116755685+JMarotoM@users.noreply.github.com>
2025-02-24 13:28:55 +0100

    Corregidos errores de duplicidad y varios

--- Front/src/js/components/dev/CategoryCarouselItem/CategoryItem.jsx
+++ Front/src/js/components/dev/CategoryCarouselItem/CategoryItem.jsx
@@ -2,10 +2,8 @@ import React from 'react';
 import './CategoryItem.css';
 
 export default function CategoryItem({ title, imageUrl, description }) {
-export default function CategoryItem({ title, imageUrl, description }) {
     return (
         <div className="relative category-Carrousel-container">
-            <a href={`/categorias/${title}`}>
             <a href={`/categorias/${title}`}>
                 <img
                     src={imageUrl}
@@ -16,7 +14,7 @@ export default function CategoryItem({ title, imageUrl, description }) {
             <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-custom-color text-center p-2">
                 <h2>{title}</h2>
                 <p className="description-text">{description}</p>
-                </div>
+            </div>
             </a>
         </div>
     );
--- Front/src/js/pages/CategoryPage/CategoryPage.jsx
+++ Front/src/js/pages/CategoryPage/CategoryPage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import CreatePost from "../../components/dev/createPost/createPost";
 import BackToTop from "../../components/dev/BackToTop/BackToTop";
-import ArticleFinder from "../../components/dev/article_finder/Article_finder_daisy";
+import ArticleFinder from "../../components/dev/article_finder/ArticleFinder";
 import DetallesBlog from "../../components/dev/DetallesBlog/DetallesBlog";
 import './CategoryPage.css';
 
