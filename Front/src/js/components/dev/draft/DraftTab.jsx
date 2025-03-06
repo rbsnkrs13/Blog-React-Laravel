0330b0bc25572f38dd1fab622a43fa09db5afd69
Hmg <HmgDevWorks@gmail.com>
2025-03-06 21:22:49 +0100

    Componente Editor mejorado, ahora los borradores muestran el texto en un editor

--- Front/src/js/components/dev/draft/DraftTab.jsx
+++ Front/src/js/components/dev/draft/DraftTab.jsx
@@ -2,25 +2,25 @@ import React from 'react';
 import './DraftTab.css';
 import Editor from '../editor/Editor';
 
-export default function DraftTab ({ tabs }) {
-    return (
-      <div role="tablist" className="tabs tabs-lifted draft-tabs">
+export default function DraftTab({ tabs }) {
+
+  return (
+    <div role="tablist" className="tabs tabs-lifted draft-tabs">
       {tabs.map((tab, index) => (
         <React.Fragment key={index}>
-        <input
-          type="radio"
-          name="my_drafts"
-          role="tab"
-          className="tab"
-          aria-label={tab.title.substring(0,25)}
-          defaultChecked={index === 0}
-        />
-        <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
-          <p>{tab.content.substring(0, 600)}</p>
-          {/* <Editor isEditable={false} postText={tab.content.substring(0, 600)} /> */}
-        </div>
+          <input
+            type="radio"
+            name="my_drafts"
+            role="tab"
+            className="tab"
+            aria-label={tab.title.substring(0, 25)}
+            defaultChecked={index === 0}
+          />
+          <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
+            <Editor post={tab} />
+          </div>
         </React.Fragment>
       ))}
-      </div>
-    );
-  };
+    </div>
+  );
+};
--- Front/src/js/pages/CrearBlogPage/CrearBlogPage.jsx
+++ Front/src/js/pages/CrearBlogPage/CrearBlogPage.jsx
@@ -2,74 +2,28 @@ import React from "react";
 import Box from "../../components/dev/box/Box";
 import DraftTab from "../../components/dev/draft/DraftTab";
 import { useState, useEffect } from "react";
-import axios from "axios";
-import PostTable from "../../components/dev/postsTable/PostTable";
+import PostTable from "../../components/dev/PostsTable/PostTable";
 import Editor from "../../components/dev/editor/Editor";
-import Separador from "../../components/dev/separador/Separador";
+import Separador from "../../components/dev/Separador/Separador";
 import postService from "../../services/postService";
 
 // import Separador from "../../components/dev/separador/Separador";
-const provDataDraft = [
-  {
-    title: "Tab 1",
-    content: "Tab 1 aaaaaaaaaaaaaa",
-  },
-  {
-    title: "Tab 2",
-    content: "Tab 2 aaaaaaaaaaaaaa",
-  },
-  {
-    title: "Tab 3",
-    content: "Tab 3 aaaaaaaaaaaaaa",
-  },
-  {
-    title: "Tab 3",
-    content: "Tab 343 aa aafdfsdaaaaaaaaaa",
-  },
-];
-
-const provPosts = [
-  {
-    title: "Post 1",
-    publicationDate: "2021-10-01",
-    content: "Post 1 aaaaaaaaaaaaaa",
-    reach: "Publico",
-  },
-  {
-    title: "Post 2",
-    publicationDate: "2021-10-02",
-    content: "Post 2 aaaaaaaaaaaaaa",
-    reach: "Privado",
-  },
-  {
-    title: "Post 3",
-    publicationDate: "2021-10-03",
-    content: "Post 3 aaaaaaaaaaaaaa",
-    reach: "Publico",
-  },
-  {
-    title: "Post 4",
-    publicationDate: "2021-10-04",
-    content: "Post 4 aaaaaaaaaaaaaa",
-    reach: "Privado",
-  },
-];
 
 const CrearBlogPage = () => {
-  const [dataDraft, setDataDraft] = useState(provDataDraft);
-  const [posts, setPosts] = useState(provPosts);
+  const [dataDraft, setDataDraft] = useState([]);
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     const userId = localStorage.getItem('userId');
     const request = postService.getUserPosts(userId);
 
     request
-    .then(response => {
-      setDataDraft(response.data);
-    })
-    .catch(error => {
-      console.error('Error al obtener los posts:', error);
-    });
+      .then(response => {
+        setDataDraft(response.data);
+      })
+      .catch(error => {
+        console.error('Error al obtener los posts:', error);
+      });
 
   }, []);
 
@@ -78,13 +32,13 @@ const CrearBlogPage = () => {
     const request = postService.getUserPosts(userId);
 
     request
-    .then(response => {
-      console.log('DATOS:', response.data);
-      setPosts(response.data);
-    })
-    .catch(error => {
-      console.error('Error al obtener los posts:', error);
-    });
+      .then(response => {
+        // console.log('DATOS:', response.data);
+        setPosts(response.data);
+      })
+      .catch(error => {
+        console.error('Error al obtener los posts:', error);
+      });
 
   }, []);
 
