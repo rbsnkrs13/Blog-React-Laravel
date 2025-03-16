8bd341a3bdd8efbf6d8c0a0b5b17e903e2860a87
Hmg <HmgDevWorks@gmail.com>
2025-03-16 15:13:21 +0100

    Fav Toggle Component and dependencies

---
+++ Front/src/js/components/dev/FavToggle/FavToggle.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { FaStar, FaRegStar } from 'react-icons/fa';
+import favService from '../../../services/favService';
+import { useAlert } from '../../../bootstrap/contexts/AlertContext';
+
+export default function FavToggle({ fav, id }) {
+    const { addError, addSuccess } = useAlert();
+    const [isFav, setIsFav] = useState(fav);
+    const handleToggle = (e) => {
+        e.stopPropagation(); // Previene que el click se propague a la fila
+        if (isFav) {
+            removeFav(id);
+        } else {
+            addFav(id);
+        }
+    };
+
+    const addFav = () => {
+        favService.addFav(id)
+            .then(response => {
+                console.log(response);
+                setIsFav(() => true);
+                addSuccess(response.data.mensaje);
+            }).catch(error => {
+                console.log(error);
+                addError(error.mensaje);
+            });
+    };
+
+    const removeFav = () => {
+        favService.removeFav(id)
+            .then(response => {
+                console.log(response);
+                setIsFav(() => false);
+                addSuccess(response.data.mensaje);
+            }).catch(error => {
+                console.log(error);
+                addError(error.mensaje);
+            });
+    };
+
+    return (
+        <a onClick={handleToggle} style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+        }} className='fav-toggle'>
+            {isFav ? <FaStar color="gold" /> : <FaRegStar color="gold" />}
+        </a>
+    );
+}
