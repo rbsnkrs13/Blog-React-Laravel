e354d678c920dc5e10a5dec269d704d4a12e7ace
Hmg <HmgDevWorks@gmail.com>
2025-03-22 23:34:18 +0100

    Dependencias de multilang

---
+++ Front/src/js/components/dev/LangBubble/LangBubble.jsx
@@ -0,0 +1,32 @@
+import { useTranslation } from "react-i18next";
+import { FaGlobe } from "react-icons/fa";
+
+export default function LangBubble() {
+    const { i18n } = useTranslation();
+
+    const changeLanguage = (lng) => {
+        i18n.changeLanguage(lng);
+        localStorage.setItem("language", lng); // Guarda el idioma seleccionado
+    };
+
+    return (
+        <div className="fixed top-4 right-4">
+            <div className="dropdown dropdown-top dropdown-end">
+                <label tabIndex={0} className="btn btn-circle btn-primary">
+                    <FaGlobe className="text-xl" />
+                </label>
+                <ul
+                    tabIndex={0}
+                    className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-28"
+                >
+                    <li>
+                        <button onClick={() => changeLanguage("en")}>🇺🇸 English</button>
+                    </li>
+                    <li>
+                        <button onClick={() => changeLanguage("es")}>🇪🇸 Español</button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
